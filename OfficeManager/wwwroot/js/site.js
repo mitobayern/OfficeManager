d4262adc8760102cd53e524f9160e5ef4e9c206a
Dimitar Georgiev <mitobayern@gmail.com>
2020-03-31 00:25:44 +0300

    Meters navbar nested dropdown menu

--- OfficeManager/wwwroot/js/site.js
+++ OfficeManager/wwwroot/js/site.js
@@ -1,4 +1,17 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$(document).ready(function () {
+    $('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+        if (!$(this).next().hasClass('show')) {
+            $(this).parents('.dropdown-menu').first().find('.show').removeClass("show");
+        }
+        var $subMenu = $(this).next(".dropdown-menu");
+        $subMenu.toggleClass('show');
+        $(this).toggleClass('show');
 
-// Write your JavaScript code.
+        return false;
+    });
+
+    $('#dropdown').on('hidden.bs.dropdown', function () {
+        $('#dropdown .dropdown-submenu .show').removeClass('show');
+        $('#dropdown a.dropdown-toggle').removeClass('show');
+    });
+});
