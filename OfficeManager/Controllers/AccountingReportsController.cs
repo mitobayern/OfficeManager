8894f5545faf92f7f4b6f8f273e22c3a9e007415
Dimitar Georgiev <mitobayern@gmail.com>
2020-04-02 19:03:34 +0300

    Accounting reports validation

--- OfficeManager/Controllers/AccountingReportsController.cs
+++ OfficeManager/Controllers/AccountingReportsController.cs
@@ -1,37 +1,18 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Newtonsoft.Json;
-using OfficeManager.Data;
-using OfficeManager.Services;
-using OfficeManager.ViewModels.AccountingReports;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace OfficeManager.Controllers
+﻿namespace OfficeManager.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Newtonsoft.Json;
+    using OfficeManager.Services;
+    using OfficeManager.ViewModels.AccountingReports;
+    using System.Linq;
+    using System.Threading.Tasks;
+
     public class AccountingReportsController : Controller
     {
-        private readonly ApplicationDbContext dbContext;
-        private readonly ITenantsService tenantsService;
-        private readonly ILandlordsService landlordsService;
-        private readonly IPricesInformationService pricesInformationService;
-        private readonly IMeasurementsService measurementsService;
         private readonly IAccontingReportsService accontingReportsService;
 
-        public AccountingReportsController(ApplicationDbContext dbContext,
-                                           ITenantsService tenantsService,
-                                           ILandlordsService landlordsService,
-                                           IPricesInformationService pricesInformationService,
-                                           IMeasurementsService measurementsService,
-                                           IAccontingReportsService accontingReportsService)
+        public AccountingReportsController(IAccontingReportsService accontingReportsService)
         {
-            this.dbContext = dbContext;
-            this.tenantsService = tenantsService;
-            this.landlordsService = landlordsService;
-            this.pricesInformationService = pricesInformationService;
-            this.measurementsService = measurementsService;
             this.accontingReportsService = accontingReportsService;
         }
 
@@ -41,75 +22,38 @@ namespace OfficeManager.Controllers
             {
                 Tenants = this.accontingReportsService.GetAllTenants(),
                 Periods = this.accontingReportsService.GetAllPeriods(),
+                AccountingReports = this.accontingReportsService.GetAllAccountingReports().ToList(),
             };
 
             return this.View(tenantsAndPeriods);
         }
 
+        [HttpPost]
+        public IActionResult Create(TenantsAndPeriodsViewModel input)
+        {
+            if (!ModelState.IsValid)
+            {
+                var tenantsAndPeriods = new TenantsAndPeriodsViewModel
+                {
+                    Tenant = input.Tenant,
+                    Period = input.Period,
+                    Tenants = this.accontingReportsService.GetAllTenants(),
+                    Periods = this.accontingReportsService.GetAllPeriods(),
+                    AccountingReports = this.accontingReportsService.GetAllAccountingReports().ToList(),
+                };
+
+                return this.View(tenantsAndPeriods);
+            }
+            return this.RedirectToAction("Generate", input);
+        }
+
         public IActionResult Generate(TenantsAndPeriodsViewModel input)
         {
-            //int number = 0;
-            //if (this.dbContext.AccountingReports.Count() == 0)
-            //{
-            //    number = 1;
-            //}
-            //else
-            //{
-            //    number = this.dbContext.AccountingReports.Count() + 1;
-            //}
-
-            //var landlord = this.landlordsService.GetLandlord();
-
-            //var tenant = this.tenantsService.GetTenantByCompanyName(input.Tenant);
-
-            //var pricesInformation = this.pricesInformationService.GetCurrentPrices();
-
-            //var electricityConsummation = this.accontingReportsService.GetTenantElectricityConsummationByPeriod(tenant.CompanyName, input.Period);
-            //var amountForElectricity = this.accontingReportsService.AmountForElectricity(electricityConsummation);
-            //var temperatureConsummation = this.accontingReportsService.GetTenantTemperatureConsummationByPeriod(tenant.CompanyName, input.Period);
-
-            //var amountForHeating = temperatureConsummation.HeatingConsummation * pricesInformation.HeatingPerKWh;
-            //var amountForCooling = temperatureConsummation.CoolingConsummation * pricesInformation.CoolingPerKWh;
-            //var totalAmount = (amountForElectricity + amountForHeating + amountForCooling) * 1.20M;
-
-            //var tenantInfo = new TenantAccountingReportViewModel
-            //{
-            //    Id = tenant.Id,
-            //    CompanyName = tenant.CompanyName,
-            //    Address = tenant.Address,
-            //    Bulstat = tenant.Bulstat,
-            //    EIK = this.tenantsService.GetTenantEIK(input.Tenant),
-            //    CompanyOwner = tenant.CompanyOwner,
-            //    StartOfContract = tenant.StartOfContract,
-            //    Offices = this.tenantsService.GetTenantOfficesAsText(input.Tenant),
-            //};
-
-            //AccountingReportViewModel accountingReport = new AccountingReportViewModel
-            //{
-            //    Number = number,
-            //    Landlord = landlord,
-            //    Tenant = tenantInfo,
-            //    CreatedOn = DateTime.UtcNow,
-            //    Period = input.Period,
-            //    PricesInformation = pricesInformation,
-            //    DayTimeElectricityConsummation = electricityConsummation.DayTimeConsummation,
-            //    NightTimeElectricityConsummation = electricityConsummation.NightTimeConsummation,
-            //    AmountForElectricity = this.accontingReportsService.AmountForElectricity(electricityConsummation),
-            //    HeatingConsummation = temperatureConsummation.HeatingConsummation,
-            //    AmountForHeating = amountForHeating,
-            //    CoolingConsummation = temperatureConsummation.CoolingConsummation,
-            //    AmountForCooling = amountForCooling,
-            //    PricesInformationId = pricesInformation.Id,
-            //    TenantId = tenant.Id,
-            //    TotalAmount = totalAmount,
-            //};
             var accountingReport = this.accontingReportsService.GetAccountingReportViewModel(input.Tenant, input.Period);
-
             var accountingReportJson = JsonConvert.SerializeObject(accountingReport, Formatting.Indented);
 
             return this.View(new AccountingReportWithJson { Json = accountingReportJson, AccountingReport = accountingReport });
         }
-
 
         [HttpPost]
         public IActionResult Generate(AccountingReportWithJson input)
@@ -132,7 +76,6 @@ namespace OfficeManager.Controllers
             var accountingReport = this.accontingReportsService.GetAccountingReportById(input.Id);
 
             return this.View(accountingReport);
-
         }
     }
 }
--- OfficeManager/ViewModels/AccountingReports/TenantsAndPeriodsViewModel.cs
+++ OfficeManager/ViewModels/AccountingReports/TenantsAndPeriodsViewModel.cs
@@ -1,16 +1,35 @@
 ﻿namespace OfficeManager.ViewModels.AccountingReports
 {
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.Linq;
 
-    public class TenantsAndPeriodsViewModel
+    public class TenantsAndPeriodsViewModel : IValidatableObject
     {
         public List<SelectListItem> Periods { get; set; }
 
         public List<SelectListItem> Tenants { get; set; }
 
+        [Required]
         public string Period { get; set; }
 
+        [Required]
         public string Tenant { get; set; }
+
+        [BindProperty]
+        public List<AccountingReportListViewModel> AccountingReports { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (AccountingReports != null)
+            {
+                if (AccountingReports.Any(x => x.CompanyName == Tenant && x.Period == Period))
+                {
+                    yield return new ValidationResult($"{Tenant} already has accounting report for {Period}", new List<string> { "Tenant" });
+                }
+            }
+        }
     }
 }
