8fed683ed4b7e7c26f1fed94d0924087ba253d5d
Dimitar Georgiev <mitobayern@gmail.com>
2020-04-15 12:02:30 +0300

    ChartsController added

---
+++ OfficeManager/Areas/Administration/Controllers/ChartsController.cs
@@ -0,0 +1,20 @@
+﻿namespace OfficeManager.Areas.Administration.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Area("Administration")]
+    [Authorize(Roles = "Admin")]
+    public class ChartsController : Controller
+    {
+        public IActionResult Index(int id)
+        {
+
+            return this.View(id);
+        }
+    }
+}
